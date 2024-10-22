206cdbe84e993ca6e25e5597af154a0d4e4678b9
“Ollie <ollie@beenhamow.co.uk>
2024-10-22 22:54:51 +0100

    Updated k3s init

--- k3s/k3s-init.sh
+++ k3s/k3s-init.sh
@@ -1,7 +1,7 @@
 k3sup install \
 --ip $1 \
---tls-san 10.21.20.10 \
---tls-san k3s-cluster.lab.ljmu.dev \
+--tls-san 10.20.4.10 \
+--tls-san k3s-cluster.home.beantech.uk \
 --cluster \
 --no-extras \
 --local-path $HOME/.kube/config \
